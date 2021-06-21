65190d011624458d7c35cd0153df382a12f3be62
Aleksandr Drozdov <alexmaa1992@gmail.com>
2021-06-21 20:29:40 +0300

    Delta team first change

---
+++ src/components/Delta.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Delta = () => {
+  return (
+    <div>
+      <h1>Delta team</h1>
+      <p>Delta team works on this file</p>
+    </div>
+  );
+};
+
+export default Delta;
