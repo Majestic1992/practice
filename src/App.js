31a15a622198cc1699fc1e29c240815ea3008160
Aleksandr Drozdov <alexmaa1992@gmail.com>
2021-06-21 20:31:08 +0300

    Alpha team 3rd change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Master from "./components/Master";
+import Alpha from "./components/Alpha";
+import Beta from "./components/Beta";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey everyone!</h1>
       <Master />
+      <Alpha />
+      <Beta />
     </div>
   );
 }
