51895780bbf29a5419b3547911b13e5ece724773
Aleksandr Drozdov <alexmaa1992@gmail.com>
2021-06-21 20:21:25 +0300

    Changes by ALPHA team

--- src/components/Alpha.js
+++ src/components/Alpha.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Alpha = () => {
-  return <div>Alpha branch</div>;
+  return (
+    <div>
+      <h4>Change by Alpha team</h4>
+    </div>
+  );
 };
 
 export default Alpha;
