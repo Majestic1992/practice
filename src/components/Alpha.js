c3037f5a394952fc5b4db1ea6ca575d08481cb07
Aleksandr Drozdov <alexmaa1992@gmail.com>
2021-07-07 23:27:11 +0300

    new commit

--- src/components/Alpha.js
+++ src/components/Alpha.js
@@ -3,7 +3,7 @@ import React from "react";
 const Alpha = () => {
   return (
     <div>
-      <p>Let's do this</p>
+      <p>Alpha will handle this</p>
       <h1>Addition from master</h1>
       <h2>Two teams are working on this branch</h2>
       <h4>Change by Alpha team</h4>
